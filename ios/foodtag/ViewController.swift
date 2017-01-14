82f355a6357eabe680f6501b298d7fdda58c8302
Jack <jackgoh0@gmail.com>
2017-01-14 23:14:31 +0800

    Added image capture

---
+++ ios/foodtag/ViewController.swift
@@ -0,0 +1,33 @@
+import UIKit
+import AVFoundation
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet weak var baseView: UIView!
+    let session = AVCaptureSession()
+    let camera = CameraUtil()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupCameraView()
+        session.startRunning()
+    }
+    
+    private func setupCameraView() {
+        
+        let device = camera.findDevice(position: .back)
+        
+        if let videoLayer = camera.createView(session: session, device: device) {
+            
+            videoLayer.frame = baseView.bounds
+            videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            baseView.layer.addSublayer(videoLayer)
+        } else {
+            fatalError("VideoLayerがNil")
+        }
+    }
+    
+    @IBAction func photoDidTap(_ sender: UIButton) {
+        camera.takePhoto()
+    }
+}
