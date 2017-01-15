0ba75654b16750b980e4c61125e1be18fa24e5e6
Jack <jackgoh0@gmail.com>
2017-01-16 00:51:07 +0800

    Created new working app, now work with upload

---
+++ foodtagios/foodtagios/ViewController.swift
@@ -0,0 +1,147 @@
+//
+//  ViewController.swift
+//  foodtagios
+//
+//  Created by Jack on 15/01/2017.
+//  Copyright © 2017 Jack. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import Alamofire
+
+class ViewController: UIViewController, AVCapturePhotoCaptureDelegate  {
+
+ 
+  
+   
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var button: UIButton!
+
+    var captureSession = AVCaptureSession()
+    var sessionOutput = AVCapturePhotoOutput()
+    var previewLayer = AVCaptureVideoPreviewLayer()
+    var foodImage: UIImage!
+    
+    let URL_PATH : String = "https://ca9b4884.ngrok.io"
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let deviceSession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDuoCamera,.builtInTelephotoCamera,.builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .unspecified)
+        
+        for device in (deviceSession?.devices)! {
+            
+            if device.position == AVCaptureDevicePosition.back {
+                
+                do {
+                    
+                    let input = try AVCaptureDeviceInput(device: device)
+                    
+                    if captureSession.canAddInput(input){
+                        captureSession.addInput(input)
+                        
+                        if captureSession.canAddOutput(sessionOutput){
+                            captureSession.addOutput(sessionOutput)
+                            
+                            previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                            previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+                            previewLayer.connection.videoOrientation = .portrait
+                            
+                            cameraView.layer.addSublayer(previewLayer)
+                            //cameraView.addSubview(button)
+                            
+                            previewLayer.position = CGPoint (x: self.cameraView.frame.width / 2, y: self.cameraView.frame.height / 2)
+                            previewLayer.bounds = cameraView.frame
+                            
+                            captureSession.startRunning()
+                            
+                        }
+                    }
+                    
+                    
+                } catch let avError {
+                    print(avError)
+                }
+                
+                
+            }
+            
+        }
+        
+    }
+    
+    /** end render view **/
+
+    /** start capture **/
+    func capture(_ captureOutput: AVCapturePhotoOutput, didFinishProcessingPhotoSampleBuffer photoSampleBuffer: CMSampleBuffer?, previewPhotoSampleBuffer: CMSampleBuffer?, resolvedSettings: AVCaptureResolvedPhotoSettings, bracketSettings: AVCaptureBracketedStillImageSettings?, error: Error?) {
+        
+        if let error = error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        if let sampleBuffer = photoSampleBuffer, let previewBuffer = photoSampleBuffer, let dataImage = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: sampleBuffer, previewPhotoSampleBuffer: previewBuffer) {
+        
+                self.foodImage = UIImage(data: dataImage)
+                let data = UIImageJPEGRepresentation(self.foodImage!, 0.8)
+            
+                captureSession.stopRunning()
+                previewLayer.removeFromSuperlayer()
+                
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "results")
+                self.present(vc, animated: true, completion:  nil)
+           
+            
+            let headers: HTTPHeaders = [
+                "Origin": "http://ca9b4884.ngrok.io",
+                "Content-type": "multipart/form-data; boundary=----WebKitFormBoundaryg9qBUnBrYZZ2rZOy",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Referer": "http://ca9b4884.ngrok.io/picture"
+            ]
+            
+            Alamofire.upload(
+                multipartFormData: { multipartFormData in
+                    multipartFormData.append(data!, withName: "file",fileName: "file", mimeType: "image/jpeg")
+                   
+            },
+                to: "https://ca9b4884.ngrok.io/picture",
+                headers: headers,
+                encodingCompletion: { encodingResult in
+                    switch encodingResult {
+                    case .success(let upload, _, _):
+                        upload.responseJSON { response in
+                            debugPrint(response)
+                        }
+                    case .failure(let encodingError):
+                        print(encodingError)
+                    }
+            }
+            )
+            
+        }
+    }
+    /** end capture **/
+    
+    /** start take photo **/
+    @IBAction func takePhoto(_ sender: Any) {
+        let settings = AVCapturePhotoSettings()
+        let previewPixelType = settings.availablePreviewPhotoPixelFormatTypes.first!
+        let previewFormat = [kCVPixelBufferPixelFormatTypeKey as String : previewPixelType, kCVPixelBufferWidthKey as String : 160, kCVPixelBufferHeightKey as String : 160]
+        
+        settings.previewPhotoFormat = previewFormat
+        sessionOutput.capturePhoto(with: settings, delegate: self)
+        
+    }
+    /** end take photo **/
+
+
+}
+
--- server/app.py
+++ server/app.py
@@ -1,45 +1,70 @@
+#!flask/bin/python
+from flask import Flask
+from flask import jsonify
+from flask import abort
+from flask import make_response
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import send_from_directory
+from werkzeug import secure_filename
+import urllib2
 import os
-from flask import Flask, request, redirect, url_for
-from werkzeug.utils import secure_filename
-
-UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+import time
 
 app = Flask(__name__)
+UPLOAD_FOLDER = './uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+picsList=[]
+
+###################
+#  Download zone  #
+###################
+
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/picture', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
+        if file:
+            filename = str(time.time()).replace(".", "") + ".jpg"
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            return jsonify({'upload':True, 'name' : filename})
+
     return '''
     <!doctype html>
     <title>Upload new File</title>
     <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
+    <form action="" method=post enctype=multipart/form-data>
       <p><input type=file name=file>
          <input type=submit value=Upload>
     </form>
     '''
+@app.route('/get/picture/<string:name>', methods=['GET'])
+def send_pics(name):
+    pics = open("./files/" + name)
+    if pics:
+        return send_from_directory(app.config['UPLOAD_FOLDER'],
+                                       name)
 
+    abort(404)
+
+
+@app.errorhandler(400)
+def not_complete(error):
+    return make_response(jsonify({'error' : 'request not complete'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
+    p = path('./files')
+    for f in p.files(pattern='*.jpg'):
+        picsList.append({ 'url' : str('/show/pics/' + f.split('.')[0])})
